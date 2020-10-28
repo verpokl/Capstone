af74514588152d48f6f1a736d4873dbaa0b45b03
Edgar Moran <verpokl@gmail.com>
2020-10-27 21:03:01 -0500

    Update main.py
    add gpio setup for input

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 ## Main function.
 
 #set up GPIO
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13,GPIO.IN) #button 
+
 recordingNum = 0
 startLoc = getLocation('false')
+
 while 'true':
-    if buttonHit():
+    # if rf button activated
+    if GPIO.input(13) == 1:
         location, distance = getLocation('true')
         audio, samples = record()
         filecreation(location, distance, recordingNum, audio, sampleRate)
