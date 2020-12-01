34b2457fbe79bccba68061b26ac702d3d262b831
Edgar Moran <verpokl@gmail.com>
2020-12-01 02:04:28 -0600

    Update getLoc.py
    finalized distance code and gps

--- getLoc.py
+++ getLoc.py
@@ -1,9 +1,24 @@
 from gps import *
 import time
-
+import mpu
+# get location with two possible cases
 class GetLocation:
-    def getLocation(gps)
-    data = gpsd.next()
-    if loc['class'] == 'TPV':
-        loc = [getattr(loc,'lon'), 'unknown'], getattr(loc, 'lat', 'unknown')]
-        return loc
+    # general getlocation intended to create the starting location
+    def getLocation(gps):
+        # get gps coordinates from start location
+        data = gpsd.next() 
+        # verify gps data
+        if loc['class'] == 'TPV':
+            # pull gps data as [lon, lan]
+            loc = [getattr(loc,'lon'), 'unknown'], getattr(loc, 'lat', 'unknown')]
+            return loc
+    # getLocation that returns location an ddistance traveled startLocation from og constructor
+    def getLocation(gps, startLoc):
+        # get gps coordinates from start location
+        data = gpsd.next() 
+        # verify gps data
+        if loc['class'] == 'TPV':
+            # pull gps data as [lon, lan]
+            loc = [getattr(loc,'lon'), 'unknown'], getattr(loc, 'lat', 'unknown')]
+            # return [lon, lat], distance
+            return loc, mpu.haversine_distance(startLoc, loc) 
