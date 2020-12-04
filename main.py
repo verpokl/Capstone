cdd5e634d9226d7257d07f836f3297cd8e908f97
Edgar Moran <verpokl@gmail.com>
2020-12-04 09:11:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 # set up GPIO 13 as input
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(13,GPIO.IN) # RF button 
+GPIO.setup(13,GPIO.IN) # RF button pin 13 is input 
 
 # number of recordings saved
 recordingNum = 0
@@ -16,7 +16,6 @@ while 'true':
     # if RF button activated
     if GPIO.input(13) == 1:
         # activate getLocation method that also calulates distance
-        loc, distance = getLocation(startLoc)
         audio, samples = record()
         filecreation(location, distance, recordingNum, audio, sampleRate)
         recordingNum += 1
