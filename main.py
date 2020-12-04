5b61ecb80c41847446cdb6b8ddc4a42bfff00df1
Edgar Moran <verpokl@gmail.com>
2020-12-04 15:30:13 -0600

    Update main.py
    add getlocation missing make file

--- main.py
+++ main.py
@@ -40,6 +40,28 @@ def main():
             recordingNum += 1
 if __name__ == '__main__':
     main()
+
+# general getlocation intended to create the starting location
+def getLocation(gps):
+    # get gps coordinates from start location
+    data = gps.next() 
+    # verify gps data
+    if loc['class'] == 'TPV':
+        # pull gps data as [lon, lan]
+        loc = [getattr(loc,'lon'), 'unknown'], getattr(loc, 'lat', 'unknown')]
+        return loc
+# getLocation that returns location and distance traveled startLocation from og constructor
+def getLocation(gps, startLoc):
+    # get gps coordinates from start location
+    data = gps.next() 
+    # verify gps data
+    if loc['class'] == 'TPV':
+        # pull gps data as [lon, lan]
+        loc = [getattr(loc,'lon'), 'unknown'], getattr(loc, 'lat', 'unknown')]
+        # return [lon, lat], distance
+        time.sleep(1.0) # wait one second to debounce switch
+        return loc, mpu.haversine_distance(startLoc, loc) 
+
 # pull data from channel 7
 # general getlocation intended to create the starting location
 def record(duration): # duration of recording in minutes
