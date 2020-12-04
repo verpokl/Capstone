b5ce05bf37d77018d06420fe1eacf72f22ddd980
Edgar Moran <verpokl@gmail.com>
2020-12-04 15:12:25 -0600

    Update record.py
    finished record

--- record.py
+++ record.py
@@ -1,24 +1,32 @@
 import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+from gpiozero import MCP3008
+import spidev
+import time
+import numpy as np
 
-# create the spi bus
+# create the spi bus open bus and cs (bus, cs)
 # clk at 135 kHz for 75 ksps
-spi = busio.SPI(135000, MISO=board.MISO, MOSI=board.MOSI)
+spi = spidev.SpiDev()
+spi.open(0, 0)
+p = pyaudio.PyAudio()
 
-# chip select
-cs = digitalio.DigitalInOut(board.D5)
+# Settings (for clock speed)
+spi.max_speed_hz = 135000
 
 # create the mcp object
-mcp = MCP.MCP3008(spi, cs)
+# pull data from channel 7
+# select pin is 8 or CE0
+mcp = MCP3008(channel=7, clock_pin=11, mosi_pin=10, miso_pin=9, select_pin=8)
 
 # pull data from channel 7
-chan = AnalogIn(mcp, MCP.P7)
-
-class record:
-    # general getlocation intended to create the starting location
-    def record():
-        x = ((3.3/1024)*chan.value);
-        
+# general getlocation intended to create the starting location
+def record(duration): # duration of recording in minutes
+    start = time.time()         # start timer
+    rec = np.array([])
+    sec = 60 * duration;        # convert duration to seconds
+    while True:                 # while true record signals
+        np.append(rec, mcp.value)
+        if time.time() > start + sec : break
+    sampleRate = np.size(rec)/sec   #sample rate of recording 
+    return x, sampleRate        # return array with recording
+spi.close()
